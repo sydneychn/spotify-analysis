1a18dd426dff8fcc5a6dfca238124b5bc0e2e569
Syd <sydneychan2003@gmail.com>
2025-10-10 15:33:24 -0400

    Established connection to PostgreSQL and started matching genres to artists

--- cleaning.py
+++ cleaning.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 #combining total history
 history1 = pd.read_json('history1.json')
 history2 = pd.read_json('history2.json')
@@ -7,6 +6,5 @@ history3 = pd.read_json('history3.json')
 complete = pd.concat([history1,history2,history3])
 print(complete.columns)
 complete.drop(complete.drop(columns=['platform', 'ip_addr', 'audiobook_title', 'audiobook_uri', 'audiobook_chapter_uri', 'audiobook_chapter_title', 'offline', 'offline_timestamp']))
-
 
 
